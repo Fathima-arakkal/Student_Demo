2ff33db099f564e888fe8b40696862b33d88ba4f
Fathima Arakkal <arakkalfathima19@gmail.com>
2024-01-25 14:24:56 +0530

    Changes made in student controller.. in index added the popup for create new student

--- Student_Demo/Controllers/StudentsController.cs
+++ Student_Demo/Controllers/StudentsController.cs
@@ -1,163 +1,176 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using Student_Demo.Context;
 using Student_Demo.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace Student_Demo.Controllers
+public class StudentsController : Controller
 {
-    public class StudentsController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public StudentsController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public StudentsController(ApplicationDbContext context)
+    // GET: Students
+    public async Task<IActionResult> Index()
+    {
+        var students = await _context.Students.Include(s => s.Department).ToListAsync();
+        return View(students);
+    }
+
+    // GET: Students/Details/5
+    public async Task<IActionResult> Details(Guid? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Students
-        public async Task<IActionResult> Index()
+        var student = await _context.Students
+            .Include(s => s.Department)
+            .FirstOrDefaultAsync(m => m.StudentId == id);
+
+        if (student == null)
         {
-            var applicationDbContext = _context.Students.Include(s => s.Department);
-            return View(await applicationDbContext.ToListAsync());
+            return NotFound();
         }
 
-        // GET: Students/Details/5
-        public async Task<IActionResult> Details(Guid? id)
+        return View(student);
+    }
+
+    // GET: Students/Create
+    public IActionResult Create()
+    {
+        var departments = _context.Departments.ToList();
+        ViewBag.DepartmentId = new SelectList(departments, "DepartmentId", "DepartmentId");
+        return PartialView("_Create", new Student
         {
-            if (id == null)
+            Name = "",
+            Age = 0,
+            Gender = Gender.Male, // Assuming you have a Gender enum
+            Division = 0,
+            DepartmentId = Guid.Empty,
+            Department = new Department
             {
-                return NotFound();
-            }
-
-            var student = await _context.Students
-                .Include(s => s.Department)
-                .FirstOrDefaultAsync(m => m.StudentId == id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
-        }
-
-        // GET: Students/Create
-        public IActionResult Create()
-        {
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId");
-            return View();
-        }
-
-        // POST: Students/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("StudentId,Name,Age,Gender,Division,DepartmentId")] Student student)
-        {
-            
-                student.StudentId = Guid.NewGuid();
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", student.DepartmentId);
-            return View(student);
-        }
-
-        // GET: Students/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var student = await _context.Students.FindAsync(id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", student.DepartmentId);
-            return View(student);
-        }
-
-        // POST: Students/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("StudentId,Name,Age,Gender,Division,DepartmentId")] Student student)
-        {
-            if (id != student.StudentId)
-            {
-                return NotFound();
-            }
-
-            
-                try
+                Name = "",
+                Location = new Location
                 {
-                    _context.Update(student);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StudentExists(student.StudentId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            
-            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", student.DepartmentId);
-            return View(student);
-        }
-
-        // GET: Students/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
+                    Name = "",
+                    Students = new List<Student>()
+                },
+                Students = new List<Student>()
             }
+        });
+    }
 
-            var student = await _context.Students
-                .Include(s => s.Department)
-                .FirstOrDefaultAsync(m => m.StudentId == id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
-        }
-
-        // POST: Students/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var student = await _context.Students.FindAsync(id);
-            if (student != null)
-            {
-                _context.Students.Remove(student);
-            }
-
+    // POST: Students/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("StudentId,Name,Age,Gender,Division,DepartmentId")] Student student)
+    {
+        
+            student.StudentId = Guid.NewGuid();
+            _context.Add(student);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        
+
+        ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", student.DepartmentId);
+        return PartialView("_Create", student);
+    }
+
+
+    // GET: Students/Edit/5
+    public async Task<IActionResult> Edit(Guid? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
         }
 
-        private bool StudentExists(Guid id)
+        var student = await _context.Students.FindAsync(id);
+        if (student == null)
         {
-            return _context.Students.Any(e => e.StudentId == id);
+            return NotFound();
         }
+
+        ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", student.DepartmentId);
+        return View(student);
+    }
+
+    // POST: Students/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Guid id, [Bind("StudentId,Name,Age,Gender,Division,DepartmentId")] Student student)
+    {
+        if (id != student.StudentId)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            _context.Update(student);
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!StudentExists(student.StudentId))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return RedirectToAction(nameof(Index));
+
+
+        ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", student.DepartmentId);
+        return View(student);
+    }
+
+    // GET: Students/Delete/5
+    public async Task<IActionResult> Delete(Guid? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = await _context.Students
+            .Include(s => s.Department)
+            .FirstOrDefaultAsync(m => m.StudentId == id);
+
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return View(student);
+    }
+
+    // POST: Students/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(Guid id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student != null)
+        {
+            _context.Students.Remove(student);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+
+    private bool StudentExists(Guid id)
+    {
+        return _context.Students.Any(e => e.StudentId == id);
     }
 }
---
+++ Student_Demo/wwwroot/js/createStudent.js
@@ -0,0 +1,39 @@
+﻿<script>
+    $(document).ready(function () {
+        // Open the modal when the "Create New" button is clicked
+        $("#openCreateModal").click(function () {
+            $("#createModal").modal("show");
+        });
+
+    // Submit form using AJAX
+    $("#createStudentForm").submit(function (e) {
+        e.preventDefault();
+
+    $.ajax({
+        url: $(this).attr("action"),
+    type: $(this).attr("method"),
+    data: $(this).serialize(),
+    success: function (data) {
+                    // Check if the form submission was successful
+                    if (data.success) {
+        // Close the modal
+        $("#createModal").modal("hide");
+
+    // Redirect to the Index page after successful creation
+    window.location.href = '/Students/Index'; // Change the URL if needed
+                    } else {
+        // Handle errors if needed
+    }
+                },
+    error: function (data) {
+        // Handle errors if needed
+    }
+            });
+        });
+
+    // Close the modal when the "Close" button is clicked
+    $("#closeCreateModal").click(function () {
+        $("#createModal").modal("hide");
+        });
+    });
+</script>
